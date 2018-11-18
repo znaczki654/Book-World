84a35ff84dfd2492064ab548b5cc02f42a570628
Janek <znaczki654@wp.pl>
2018-11-18 18:17:59 +0100

    Zmiany #28
    Chęć sprawdzenia czegoś na stack overflow

--- JS/slider.js
+++ JS/slider.js
@@ -1,34 +1,34 @@
-	var numer = Math.floor(Math.random()*5)+1;
-			
-			var timer1 = 0;
-			var timer2 = 0;
-			
-			function ustawslajd(nrslajdu)
-			{
-				clearTimeout(timer1);
-				clearTimeout(timer2);
-				numer = nrslajdu - 1;
-				
-				schowaj();
-				setTimeout("zmienslajd()", 500);
-				
-			}
-			
-			function schowaj()
-			{
-				$("#slider").fadeOut(500);
-			}
-		
-			function zmienslajd()
-			{
-				numer++; if (numer>5) numer=1;
-				
-				var plik = "<img src=\"img/slide" + numer + ".png\" />";
-				
-				document.getElementById("slider").innerHTML = plik;
-				$("#slider").fadeIn(500);
-				
-				timer1 = setTimeout("zmienslajd()", 5000);
-				timer2 = setTimeout("schowaj()", 4500);
-			
-			}
+var numer = Math.floor(Math.random()*5+1);
+        
+        var timer1 = 0;
+        var timer2 = 0;
+        var timer3 = 0;
+        
+        function ustawslajd(nrslajdu)
+        {
+            clearTimeout(timer1);
+            clearTimeout(timer2);
+            clearTimeout(timer3);
+            numer = nrslajdu - 1;
+            
+            schowaj();
+            timer3=setTimeout("zmienslajd()", 500);
+        }
+        
+        function schowaj()
+        {
+            $("#slider").fadeOut(500);
+        }
+        
+        function zmienslajd()
+        {
+            numer++; if(numer>5) numer=1;
+            
+            var plik = "<img src=\"img/slide"+numer+ ".png\">";
+            
+            document.getElementById("slider").innerHTML=plik;
+            $("#slider").fadeIn(500);
+            
+            timer1=setTimeout("zmienslajd()", 5000);
+            timer2=setTimeout("schowaj()", 4500);
+        }
